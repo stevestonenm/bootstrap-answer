625ff55c5dc9c65e257dac1b4ae91fe3941a5055
Steve Stone <stoneflynm@gmail.com>
2017-08-07 14:22:28 -0600

    trying to keep up with rochelle

---
+++ index.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<!--custom css-->
+		<link rel="stylesheet" href="css/style.css">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery - required for Bootstrap Components -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<title>Bootstrap Answer</title>
+	</head>
+	<body>
+
+		<header>
+			<div class="container">
+				<nav class="navbar navbar-default">
+					<div class="container-fluid">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="#">Brand</a>
+						</div>
+
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav">
+								<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+								<li><a href="#">Link</a></li>
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+									<ul class="dropdown-menu">
+										<li><a href="#">Action</a></li>
+										<li><a href="#">Another action</a></li>
+										<li><a href="#">Something else here</a></li>
+										<li role="separator" class="divider"></li>
+										<li><a href="#">Separated link</a></li>
+										<li role="separator" class="divider"></li>
+										<li><a href="#">One more separated link</a></li>
+									</ul>
+								</li>
+							</ul>
+							<form class="navbar-form navbar-left">
+								<div class="form-group">
+									<input type="text" class="form-control" placeholder="Search">
+								</div>
+								<button type="submit" class="btn btn-default">Submit</button>
+							</form>
+							<ul class="nav navbar-nav navbar-right">
+								<li><a href="#">Link</a></li>
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+									<ul class="dropdown-menu">
+										<li><a href="#">Action</a></li>
+										<li><a href="#">Another action</a></li>
+										<li><a href="#">Something else here</a></li>
+										<li role="separator" class="divider"></li>
+										<li><a href="#">Separated link</a></li>
+									</ul>
+								</li>
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</div><!-- /.container-fluid -->
+				</nav>
+			</div>
+		</header>
+
+		<section>
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12">
+						<div class="jumbotron">
+							<h1>Hello World</h1>
+							<p>VHS vape hoodie subway tile kombucha. Slow-carb raclette pug thundercats authentic health goth. Pinterest kale chips blue bottle shoreditch artisan chillwave crucifix fanny pack selvage plaid kitsch yr. PBR&B heirloom lumbersexual, hoodie helvetica butcher enamel pin. Pitchfork taxidermy shoreditch, PBR&B wayfarers portland skateboard austin distillery pop-up lyft woke craft beer. .</p>
+							<button class="btn btn-primary">Click Me</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+		<section>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3">
+						<img src="https://www.fillmurray.com/225/225" alt="2000's Bill Murray" class="img-responsive">
+					</div>
+					<div class="col-md-9">
+						Austin truffaut 90's, etsy beard cronut lumbersexual subway tile DIY twee helvetica hoodie 8-bit. Godard bespoke taxidermy cornhole shabby chic sriracha. Irony taiyaki raclette kickstarter palo santo copper mug lyft mumblecore pitchfork VHS. IPhone shabby chic street art affogato 3 wolf moon echo park selfies you probably haven't heard of them. Vice intelligentsia chicharrones sustainable quinoa single-origin coffee, coloring book four dollar toast gentrify tbh affogato activated charcoal wolf succulents squid. Snackwave mixtape mumblecore, 3 wolf moon cronut flexitarian tousled trust fund wayfarers ennui DIY sartorial craft beer church-key. Snackwave bitters chicharrones deep v waistcoat, echo park forage. Viral selfies kinfolk semiotics. Tilde woke forage XOXO iceland, polaroid master cleanse affogato DIY.</p>
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+		<section>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3">
+						<img src="https://www.fillmurray.com/250/250" class="img-circle center-block" class="img-responsive" alt="classy">
+						<div class="well text-center">
+							<p>Truffaut blue bottle fixie before they sold out, quinoa hashtag gochujang biodiesel. Af next level cronut kale chips slow-carb bushwick, before they sold out swag kinfolk. DIY keffiyeh man braid shabby chic vice vinyl vaporware subway tile leggings hammock. Small batch succulents swag you probably haven't heard of them subway tile. Wolf readymade tacos bitters synth. </p>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<img src="https://www.fillmurray.com/250/250" class="img-circle center-block" class="img-responsive" alt="classy">
+						<div class="well text-center">
+							<p>Truffaut blue bottle fixie before they sold out, quinoa hashtag gochujang biodiesel. Af next level cronut kale chips slow-carb bushwick, before they sold out swag kinfolk. DIY keffiyeh man braid shabby chic vice vinyl vaporware subway tile leggings hammock. Small batch succulents swag you probably haven't heard of them subway tile. Wolf readymade tacos bitters synth.</p>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<img src="https://www.fillmurray.com/250/250" class="img-circle center-block" class="img-responsive" alt="classy">
+						<div class="well text-center">
+							<p>Truffaut blue bottle fixie before they sold out, quinoa hashtag gochujang biodiesel. Af next level cronut kale chips slow-carb bushwick, before they sold out swag kinfolk. DIY keffiyeh man braid shabby chic vice vinyl vaporware subway tile leggings hammock. Small batch succulents swag you probably haven't heard of them subway tile. Wolf readymade tacos bitters synth.</p>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<img src="https://www.fillmurray.com/250/250" class="img-circle center-block" class="img-responsive" alt="classy">
+						<div class="well text-center">
+							<p>Truffaut blue bottle fixie before they sold out, quinoa hashtag gochujang biodiesel. Af next level cronut kale chips slow-carb bushwick, before they sold out swag kinfolk. DIY keffiyeh man braid shabby chic vice vinyl vaporware subway tile leggings hammock. Small batch succulents swag you probably haven't heard of them subway tile. Wolf readymade tacos bitters synth.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<p>This is a footer</p>
+					</div>
+				</div>
+			</div>
+		</footer>
+
+	</body>
+</html>
